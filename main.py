ff1075755771aea89ace914f2a0b3573f56907e4
Erfan-ram <Erfanramezany245@gmail.com>
2023-03-03 17:16:27 +0330

    implanted duration calculating

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # created
 import Algorithms as alg
+from time import time_ns as record
 
 
 class main():
@@ -11,8 +12,25 @@ class main():
 
         print("then i will create a dataset contain each numbers execute duration time for both algorithms")
 
+        self.number = int(input("\n\n calculating Fibonacci from 0 to :  "))
+
     def Fibonacci(self, num):
-        print(alg.devide_conquer(num), alg.dynamic_programming(num))
+        calculated_time = []
+
+        for nums in range(0, num+1):
+            start = record()
+            alg.devide_conquer(nums)
+            end = record()
+            devide_duration = end-start
+
+            start = record()
+            alg.dynamic_programming(nums)
+            end = record()
+            dynamic_duration = end-start
+
+            calculated_time.append[num, devide_duration, dynamic_duration]
+            
+            return calculated_time
 
 
 program = main()
