8d9909d2aa2c1a8908d27224f642b5d052459c9d
Erfan-ram <Erfanramezany245@gmail.com>
2023-03-03 16:48:37 +0330

    dynamic implanted

--- Algorithms.py
+++ Algorithms.py
@@ -4,3 +4,11 @@ def devide_conquer(number):
 
     else:
         return devide_conquer(number-1)+devide_conquer(number-2)
+
+
+def dynamic_programming(number):
+    f = [0, 1]
+
+    for i in range(2, number+1):
+        f.append(f[i-1] + f[i-2])
+    return f[number]
