61d48dee27c513a2a62e8de63881f8fbe074cbb4
Erfan-ram <Erfanramezany245@gmail.com>
2023-03-03 16:37:44 +0330

    class init complited

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 # created
+
+class main():
+    def __init__(self) -> None:
+        print("""Welcome
+              i am a program to solve Fibonacci using two algorithms :
+              Divide and Conquer Algorithms 
+              Dynamic Programming""")
+
+        print("then i will create a dataset contain each numbers execute duration time for both algorithms")
+
+    def start(self):
+        pass
